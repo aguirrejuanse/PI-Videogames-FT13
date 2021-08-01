f61fd46874d5d4e37a30502f2eb8966e67a28ed8
aguirrejuanse <you@example.com>
2021-07-31 22:52:56 -0300

    add axios in constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,5 @@
-export const BASE_URL = 'http://localhost:3001/api/';
+import axios from 'axios';
+
+export const BASE_URL = axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001/api';
 export const GAME_URL = `${BASE_URL}videogames/`;
 export const GENRE_URL = `${BASE_URL}genres/`
